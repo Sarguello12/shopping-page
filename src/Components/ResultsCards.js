692cf7b85565050d83861fbf758a2674cfe8b3d0
Samuel Arguello <sam.rene12@gmail.com>
2022-07-17 19:49:35 -0500

    fix: added line through on msrp and thumbnail images

--- src/Components/ResultsCards.js
+++ src/Components/ResultsCards.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ResultsCards = (props) => {
   return (
     <div>
-      <img src={props.imageUrl}></img>
+      <img src={props.thumbnailImageUrl}></img>
       <p>{props.name}</p>
-      <p>${props.price}</p>
+
+      {props.msrp > props.price ? (
+        <div>
+          <p className="msrp">${props.msrp}</p>
+          <p>${props.price}</p>
+        </div>
+      ) : (
+        <div>
+          <p>${props.price}</p>
+        </div>
+      )}
     </div>
   );
 };
